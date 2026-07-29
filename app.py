99a180784e2ebbb070f04507221a6d6539a369ae
danielmacneil-cpu <daniel.macneil@dmacneil.us>
2026-07-28 20:43:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -218,9 +218,10 @@ elif page == "Admin Document Generator":
                         ("templates/person/Order_AAL.docx", f"03_Order_Appointing_AAL_{ward_name}.docx"),
                         ("templates/person/Affidavit_Notice.docx", f"04_Affidavit_Regarding_Notice_{ward_name}.docx"),
                         ("templates/person/Waiver_Notice.docx", f"05_Waiver_of_Notice_{ward_name}.docx"),
-                        ("templates/person_and_estate/Inventory_Appraisement.docx", f"06_Inventory_Appraisement_{ward_name}.docx"),
-                        ("templates/person_and_estate/Order_Approving_Inventory.docx", f"07_Order_Approving_Inventory_{ward_name}.docx"),
-                        ("templates/person/Oath.docx", f"08_Oath_of_Guardian_{ward_name}.docx")
+                        ("templates/person_and_estate/Order_Guardianship_Person_Estate.docx", f"06_Order_Appointing_Permanent_Guardian_{ward_name}.docx"),
+                        ("templates/person_and_estate/Inventory_Appraisement.docx", f"07_Inventory_Appraisement_{ward_name}.docx"),
+                        ("templates/person_and_estate/Order_Approving_Inventory.docx", f"08_Order_Approving_Inventory_{ward_name}.docx"),
+                        ("templates/person/Oath.docx", f"09_Oath_of_Guardian_{ward_name}.docx")
                     ]
 
                     zip_buffer = io.BytesIO()
